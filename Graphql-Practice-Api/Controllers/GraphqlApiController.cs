442789f5fb5b2bad6b483afe797b8958419ae5e2
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-08-12 23:06:59 +0430

    GraphqlApiController

---
+++ Graphql-Practice-Api/Controllers/GraphqlApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Graphql_Practice_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphqlApiController : ControllerBase
+    {
+    }
+}
